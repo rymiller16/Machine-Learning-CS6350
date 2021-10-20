3ce51e1a7e40a4ec47590ca98ee3dcdb957cd37e
rymiller16 <90581880+rymiller16@users.noreply.github.com>
2021-10-20 16:04:23 -0700

    Delete entropy_by4.m

--- DecisionTree/entropy_by4.m
+++
@@ -1,8 +0,0 @@
-function stuff = entropy_by4(n1,n2,n3,n4)
-    total = n1+n2+n3+n4;
-    if total == 0
-        stuff = 0;
-    else
-        stuff = (-n1/total)*log4(n1/total)-(n2/total)*log4(n2/total)-(n3/total)*log4(n3/total)-(n4/total)*log4(n4/total);
-    end 
-end
