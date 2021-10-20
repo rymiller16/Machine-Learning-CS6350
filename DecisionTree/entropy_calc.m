bb2f059ef5f459206778a7f976aa715a6938c732
rymiller16 <90581880+rymiller16@users.noreply.github.com>
2021-10-20 16:04:28 -0700

    Delete entropy_calc.m

--- DecisionTree/entropy_calc.m
+++
@@ -1,9 +0,0 @@
-function stuff = entropy_DS(T)
-    total = count1+count2+count3+count4;
-    entropy = (-count1/total)*log4(count1/total) - (count2/total)*log4(count2/total) - (count3/total)*log4(count3/total) - (count4/total)*log4(count4/total); 
-    stuff = entropy;
-end 
-
-    
-    
-    
